696305168716531b1c6488208986ef656dcaeeb5
pablomunozSandoval <k_pablo17@hotmail.com>
2024-12-09 10:47:36 -0300

    Configuracion Firebase

--- Reserv/models.py
+++ Reserv/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from app.firebase_config import db  # Importar firebase_config usando una importación absoluta
 
 class Doctor(models.Model):
     name = models.CharField(max_length=100)
@@ -8,6 +9,17 @@ class Doctor(models.Model):
     schedule = models.CharField(max_length=100)
     available = models.BooleanField(default=True)
 
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        doc_ref = db.collection('doctors').document(str(self.id))
+        doc_ref.set({
+            'name': self.name,
+            'specialty': self.specialty,
+            'description': self.description,
+            'schedule': self.schedule,
+            'available': self.available
+        })
+
     def __str__(self):
         return self.name
 
@@ -15,6 +27,15 @@ class Appointment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        doc_ref = db.collection('appointments').document(str(self.id))
+        doc_ref.set({
+            'user': self.user.username,
+            'doctor': self.doctor.name,
+            'date_created': self.date_created
+        })
 
     def __str__(self):
         return f'Cita con {self.doctor.name} por {self.user.username}'
